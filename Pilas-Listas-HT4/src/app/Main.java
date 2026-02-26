4bd79253036be7a3f67510d0c786dcf00f614a8c
JunicGames <109977989+JunicGames@users.noreply.github.com>
2026-02-25 20:02:25 -0600

    Primeras clases

--- Pilas-Listas-HT4/src/app/Main.java
+++ Pilas-Listas-HT4/src/app/Main.java
@@ -6,6 +6,7 @@ import Util.ExpressionConverter;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,16 +15,38 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Tipo de Stack (arraylist, vector, lista): ");
-        String stackType = scanner.nextLine();
+        System.out.println("Tipo de Stack");
+        System.out.println("1.) lista");
+        System.out.println("2.) vector");
+        System.out.println("3.) arraylist");
+        int stackType = scanner.nextInt();
 
-        String listType = "";
+        switch (stackType) {
+            case 1: System.out.println("tipo de lista");
+                    System.out.println("1.) simple");
+                    System.out.println("2.) encadenada");
+                    System.out.println("2.) doblemente encadenada");
+                    int listType = scanner.nextInt();
 
-        if (stackType.equalsIgnoreCase("lista")) {
-            System.out.println("Tipo de Lista (simple, doble): ");
-            listType = scanner.nextLine();
+                    switch (listType) {
+
+                        case 1:
+
+                        case 2:
+
+                        case 3:
+
+                        default:
+                    }
+
+            case 2: System.out.println("tipo de arraylist a escoger");
+
+            case 3: System.out.println("tipo de stack a escoger");
+
+
+            default:
         }
-
+        }
         String expression = Files.readString(Paths.get("datos.txt")).trim();
 
         Stack<Character> stackChar =
---
+++ Pilas-Listas-HT4/test/List/ListAbstractTest.java
@@ -0,0 +1,8 @@
+package List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListAbstractTest {
+
+}
+
